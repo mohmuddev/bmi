18aebcd36682f8b374043047d74c5dbda2c62878
Mohamud <info@mohamud.dev>
2020-07-17 21:37:08 +0300

    add output result

--- app.js
+++ app.js
@@ -9,4 +9,11 @@ btn.addEventListener('click', function(){
 
     //calculate BMI
     const BMI = mass / (height * height);
+     
+    // round the BMI decimal number into one decimal place
+     const result = BMI.toFixed(1);
+
+
+    //Output BMI result
+    document.querySelector("#output").innerHTML = "Your BMI is " + " " + result;
 });
